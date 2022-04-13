a914ab6d6cc8a67271ae37b03a1aa6f529fdca00
Benjamin271201 <thaikhucngoc@gmail.com>
2022-04-13 18:04:02 +0700

    Add Swagger + migrate db to AWS

--- src/main/java/com/nashtech/ecommerce/EcommerceApplication.java
+++ src/main/java/com/nashtech/ecommerce/EcommerceApplication.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
-
 public class EcommerceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
     }
-
+//    @Bean
+//    public Docket productApi() {
+//        return new Docket(DocumentationType.SWAGGER_2).select()
+//                .apis(RequestHandlerSelectors.basePackage("com.nashtech.ecommerce")).build();
+//    }
 }
